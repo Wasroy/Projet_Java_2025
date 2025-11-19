a0882d9f7fca906015e8513e955ff6ac8b492d3a
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-11-19 09:08:44 +0100

    Add files via upload

---
+++ Projet/src/main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import solveur.glouton.GloutonAjoutSolver;
+import solveur.glouton.OrdreObjetsAjoutPremier;
+import solveur.glouton.OrdreObjetsAjoutDeuxieme;
+import sacADos.*;
+
+public static void main(String[] args) {
+        Objet o1 = new Objet(12, new int[]{3, 5});
+        Objet o2 = new Objet(7, new int[]{2, 4});
+        Objet o3 = new Objet(10, new int[]{4, 6});
+        Objet o4 = new Objet(15, new int[]{5, 7});
+        Objet o5 = new Objet(10, new int[]{3, 5});
+        
+        List<Objet> objets = new ArrayList<>();
+        objets.add(o1);
+        objets.add(o2);
+        objets.add(o3);
+        objets.add(o4);
+        objets.add(o5);
+
+
+        int[] budgets = {10, 12};
+        SacADos sac = new SacADos(2, budgets, objets);
+
+        sac.afficherSacADos();
+
+        List<Objet> resultatGloutonAjoutPremier = GloutonAjoutSolver.methodeGloutonneAjout(sac, new OrdreObjetsAjoutPremier()); //marche aussi pour le 2e critere avec new OrdreObjetsAjoutDeuxieme()
+        
+        System.out.println("--------------------------------");
+        System.out.println("Liste des objets selectionnes par la methode gloutonne a ajout premier :");
+        for (Objet o : resultatGloutonAjoutPremier) {
+            o.afficherObjet();
+        }
+		  
+		  List<Objet> resultatGloutonAjoutDeuxieme = GloutonAjoutSolver.methodeGloutonneAjout(sac, new OrdreObjetsAjoutDeuxieme()); //marche aussi pour le 2e critere avec new OrdreObjetsAjoutDeuxieme()
+		  System.out.println("--------------------------------");
+        System.out.println("Liste des objets selectionnes par la methode gloutonne a ajout deuxieme :");
+        for (Objet o : resultatGloutonAjoutDeuxieme) {
+            o.afficherObjet();
+        }
+
+    }
