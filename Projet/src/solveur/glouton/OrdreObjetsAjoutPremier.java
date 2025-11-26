301048a1b6e5b565b13376943e75ae0e040ca7fa
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-11-26 10:12:48 +0100

    Modifier commentaires

--- Projet/src/solveur/glouton/OrdreObjetsAjoutPremier.java
+++ Projet/src/solveur/glouton/OrdreObjetsAjoutPremier.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class OrdreObjetsAjoutPremier implements Comparator<Objet> {
 	
-	public static int sommeCouts(Objet o1){ //on le refait propre mais on aurait pu utiliser la fct getCoutTotal() de la classe Objet
+	public static int sommeCouts(Objet o1){ //refait propre mais on aurait pu utiliser la fct getCoutTotal() de la classe Objet
 		int somme = 0;
 		for (int i = 0; i < o1.getCouts().length; i++){
 			somme += o1.getCouts()[i];
@@ -13,7 +13,7 @@ public class OrdreObjetsAjoutPremier implements Comparator<Objet> {
 	}
 
 	@Override
-	public int compare(Objet o1, Objet o2){ //en gros on compare en coef du ratio utilite/cout total
+	public int compare(Objet o1, Objet o2){ //on compare en coef du ratio utilite/cout total
 		double f1 = (double) o1.getUtilite()/(sommeCouts(o1));
 		double f2 = (double) o2.getUtilite()/(sommeCouts(o2));
 
