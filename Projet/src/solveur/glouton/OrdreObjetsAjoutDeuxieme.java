e451d32ae85258db199ae46767aea8ec4b5aef3c
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-12-16 22:03:37 +0100

    Javadoc

--- Projet/src/solveur/glouton/OrdreObjetsAjoutDeuxieme.java
+++ Projet/src/solveur/glouton/OrdreObjetsAjoutDeuxieme.java
@@ -3,7 +3,16 @@ import sacADos.Objet;
 import java.util.Comparator;
 import java.lang.Exception;
 
+/** deuxieme critere d'ordre pour la methode gloutonne a ajout
+* @author Nathalie Habib
+*/
+
 public class OrdreObjetsAjoutDeuxieme implements Comparator<Objet> {
+
+/** renvoie le maximum des couts d'un objet
+* @param o1 objet du sac a dos
+* @return max le maximum des couts d'un objet
+*/	
 	
 	public static int maxCouts(Objet o1){
 		int max = o1.getCouts()[0];
@@ -14,6 +23,13 @@ public class OrdreObjetsAjoutDeuxieme implements Comparator<Objet> {
 		return max;
 	}
 
+/** comparer les objets selon le deuxieme critere de comparaison
+* @param o1 premier objet
+* @param o2 deuxieme objet
+* @return 0, 1 ou -1 selon l'objet le plus interessant obtneu apres comparaison
+* @throws ArithmeticException si division par 0 (si le maximum des couts est null)
+*/
+	
 	@Override
 	public int compare(Objet o1, Objet o2){		
 		try{
