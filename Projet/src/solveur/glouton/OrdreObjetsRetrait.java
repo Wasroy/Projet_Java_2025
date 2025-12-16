b9da032143aa442154cf59dd5bd6b477552d36d0
NathalieHB <102826292+xWNx@users.noreply.github.com>
2025-12-16 22:19:09 +0100

    Javadoc

--- Projet/src/solveur/glouton/OrdreObjetsRetrait.java
+++ Projet/src/solveur/glouton/OrdreObjetsRetrait.java
@@ -3,6 +3,10 @@ import sacADos.*;
 import java.util.Comparator;
 import java.lang.Exception;
 
+/** critere d'ordre methode gloutonne a retrait
+* @author William Miserolle Nathalie Habib 
+*/
+
 public class OrdreObjetsRetrait implements Comparator<Objet> {
 	
 	private final SacADos sac;
@@ -10,7 +14,11 @@ public class OrdreObjetsRetrait implements Comparator<Objet> {
 	public OrdreObjetsRetrait(SacADos sac) {
 		this.sac = sac;
 	}
-	
+
+/** determine la dimension ayant la plus gros depassement de budget
+* @return dimensionMaxDepassement la dimension ayant le plus depassement de budget
+*/	
+
 	private int trouverDimensionMaxDepassement() {
 		int dimensionMaxDepassement = 0;
 		int maxDepassementBudget = 0;
@@ -29,12 +37,23 @@ public class OrdreObjetsRetrait implements Comparator<Objet> {
 		}
 		return dimensionMaxDepassement;
 	}
+
+/** renvoie le maximum des couts pour la dimension ayant le plus gros depassement de budget
+* @param o un objet du sac a dos
+* @return o.getCouts()[dimensionProblematique] le cout en question
+*/	
 	
 	private int maxCoutObjetDimension(Objet o) {
 		int dimensionProblematique = trouverDimensionMaxDepassement();
 		return o.getCouts()[dimensionProblematique];
 	}
 
+/** critere de comparaison retrait avec exceptions
+* @param o1 premier objet
+* @param o2 deuxieme objet
+* @return 0, 1 ou -1 selon l'objet le plus interessant trouve
+*/
+	
 	@Override
 	public int compare(Objet o1, Objet o2) {
 		try{
