f9c846d9396a05cf4c2399c3ad7e567395080298
romane-fyn <romane.fayon41@gmail.com>
2025-12-19 19:09:19 +0100

    modification suite aux modifs sur le cycle

--- Projet/src/tests/EquipeMunicipaleTest.java
+++ Projet/src/tests/EquipeMunicipaleTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import equipe.*;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,25 +26,15 @@ public class EquipeMunicipaleTest {
     
     @Test
     public void testCycle() {
-    		Secteur[] s1 = {Secteur.CULTURE, Secteur.ATTRACTIVITE};
-    		Secteur[] s2 = {Secteur.SANTE, Secteur.EDUCATION};
-    		Projet p1 = new Projet("Projet1", "description du projet 1" , Secteur.SANTE, 0);
-        Projet p2 = new Projet("Projet2", "description du projet 2", Secteur.EDUCATION,0);
-        Expert ex1= new Expert("Einstein", "Albert", 63, s1 );
-        	Expert ex2=new Expert("Gauss", "Carl", 89, s2);
-        List<Expert> experts = new ArrayList<>();
-        experts.add(ex1);
-        experts.add(ex2);
-        equipeUnderTest.setExperts(experts);
-        Evaluateur eco=new Evaluateur("Newton","Isaac", 33, Specialisation.ECONOMIE);
-        	Evaluateur soc=new Evaluateur("Kepler","Johannes",58,Specialisation.SOCIAL);
-        	Evaluateur env=new Evaluateur("Copernic","Nicolas", 24, Specialisation.ENVIRONNEMENT);
-        	Elu elu = new Elu("Descartes","René", 44);
-        	equipeUnderTest.setEvaluateurs(eco, soc, env);
-        equipeUnderTest.setElu(elu);
-        equipeUnderTest.Cycle();
+        equipeUnderTest.cycle(5);
         List<Projet> complets = equipeUnderTest.getProjetsComplets();
-        assertEquals(2, complets.size(), "Il devrait y avoir deux projets finalisés");
+        for (Projet p : complets) {
+            assertNotNull(p.getBenefice(), "Le bénéfice doit être évalué");
+            assertNotNull(p.getCout(Specialisation.ECONOMIE), "Le coût économique doit être évalué");
+            assertNotNull(p.getCout(Specialisation.SOCIAL), "Le coût social doit être évalué");
+            assertNotNull(p.getCout(Specialisation.ENVIRONNEMENT), "Le coût environnemental doit être évalué");
+        }
+        assertEquals(5, complets.size(), "Il devrait y avoir deux projets finalisés");
     }
     
     
